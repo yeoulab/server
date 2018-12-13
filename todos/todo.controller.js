94391dd4fa9a3e69897212dba8879bc6d61d2a30
unknown <myungjin.shin@gmail.com>
2018-12-14 07:45:22 +0900

    complete model added

--- todos/todo.controller.js
+++ todos/todo.controller.js
@@ -4,6 +4,8 @@ const todoService = require('./todo.service');
 
 router.post('/create', createTodo);
 router.get('/:id', getByUserId);
+router.post('/complete', completeTodo);
+router.get('/:id/:dt', getCompleteByUserId);
 
 module.exports = router;
 
@@ -19,5 +21,19 @@ function getByUserId(req, res, next) {
     console.log("getByUserId Start");
     console.log(req.params.id);
     todoService.getByUserId(req.params.id).then(todos => res.json(todos))
+    .catch(err => next(err));;
+}
+
+function completeTodo(req, res, next) {
+    console.log("completeTodo Start");
+    todoService.completeTodo(req.body)
+        .then(() => res.json({}))
+        .catch(err => next(err));
+}
+
+function getCompleteByUserId(req, res, next) {
+    console.log("getByUserId Start");
+    console.log(req.params.id);
+    todoService.getByUserId(req.params.id, req.params.dt).then(todos => res.json(todos))
     .catch(err => next(err));;
 }
--- todos/todo.service.js
+++ todos/todo.service.js
@@ -3,13 +3,16 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const db = require('_helpers/db');
 const Todo = db.Todo;
+const Completed = db.Completed;
 
 // nest.js 의 injectable 과 비슷한 느낌인가??
 // function name 들을 export 함..
 // 아마도 controller 에서 사용할듯
 module.exports = {
     createTodo,
-    getByUserId
+    getByUserId,
+    completeTodo,
+    getCompleteByUserId,
 };
 
 async function createTodo(todoParam) {
@@ -33,4 +36,28 @@ async function getByUserId(userId){
     console.log("todo.service.js -> getByUserId Start");
     console.log(userId);
     return await Todo.find({ userId: userId }).select();
+}
+
+async function completeTodo(todoParam) {
+    // validate
+    console.log("create Todo Start");
+    console.log(todoParam);
+    console.log(todoParam.todo.todoName);
+    if (await Completed.findOne({ todoId: todoParam.todo.todoId
+                           , completedDate: todoParam.todo.completedDate })) {
+        // 존재하면 에러처리
+        console.log("중복존재");
+        throw 'Todoname "' + todoParam.todoParam.todo.todoName + '" is already taken';
+    }
+    console.log(todoParam.todo);
+    const completedTodo = new Completed(todoParam.todo);
+    console.log(completedTodo);
+
+    await completedTodo.save();
+}
+
+async function getCompleteByUserId(id, dt){
+    console.log("getCompletedTodo Start");
+    console.log(id + " ---- " + dt);
+    return await Completed.find({ userId: id, completedDate: dt }).select();
 }
